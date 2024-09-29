9c5166ae9da75437dcca2b9c5cfbdc4011ae2122
kevenallan <kevenallan98@gmail.com>
2024-09-29 09:46:51 -0300

    Removendo arquivos de Teste

--- src/app/shared/pipe/local-date-time-format.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { LocalDateTimeFormatPipe } from './local-date-time-format.pipe';
-
-describe('LocalDateTimeFormatPipe', () => {
-  it('create an instance', () => {
-    const pipe = new LocalDateTimeFormatPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
