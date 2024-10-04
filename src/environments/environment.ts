ad0801d6348f00903310ce434cfc2908c5167c02
kevenallan <kevenallan98@gmail.com>
2024-10-04 20:53:27 -0300

    Deploy firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,2 +1,3 @@
 // export const urlBackEnd = 'http://localhost:8080/sharebox';
-export const urlBackEnd = ' http://192.168.0.5:8080/sharebox';
+// export const urlBackEnd = ' http://192.168.0.5:8080/sharebox';
+export const urlBackEnd = 'https://shareboxback.onrender.com/sharebox';
