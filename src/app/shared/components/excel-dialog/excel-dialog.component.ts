6e91467a2fbf2e0e226edd1db27b2462abcfebeb
kevenallan <kevenallan98@gmail.com>
2024-11-11 14:07:21 -0300

    Adicionando TabView, ajuste na edição de celula e na leitura do excel

--- src/app/shared/components/excel-dialog/excel-dialog.component.ts
+++ src/app/shared/components/excel-dialog/excel-dialog.component.ts
@@ -1,5 +1,4 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-
 import { DialogModule } from 'primeng/dialog';
 import { ButtonModule } from 'primeng/button';
 import { Arquivo } from '../../../core/models/arquivo.model';
@@ -9,6 +8,7 @@ import { ArquivoService } from '../../../core/services/arquivo.service';
 import { TableModule } from 'primeng/table';
 import { FormsModule } from '@angular/forms';
 import { AlertService } from '../../../core/services/alert.service';
+import { TabViewModule } from 'primeng/tabview';
 
 @Component({
     selector: 'app-excel-dialog',
@@ -18,7 +18,8 @@ import { AlertService } from '../../../core/services/alert.service';
         FormsModule,
         DialogModule,
         ButtonModule,
-        TableModule
+        TableModule,
+        TabViewModule
     ],
     templateUrl: './excel-dialog.component.html',
     styleUrl: './excel-dialog.component.scss'
@@ -30,9 +31,9 @@ export class ExcelDialogComponent {
     arquivo?: Arquivo;
     arquivoList: Arquivo[] = [];
     mimeType?: string;
-
-    excelData: any[][] = [];
-    celulaEmEdicao: any[][] = [];
+    abaSelecionada: number = 0;
+    excelData: { sheetName: string; data: any[][] }[] = [];
+    celulaEmEdicao: { sheetName: string; data: any[][] }[] = [];
     isEditar: boolean = true;
 
     @Output() atualizarTabela = new EventEmitter();
@@ -41,8 +42,7 @@ export class ExcelDialogComponent {
         private arquivoService: ArquivoService,
         private alertService: AlertService
     ) {
-        this.celulaEmEdicao = [];
-        this.isEditar = true;
+        this.abaSelecionada = 0;
     }
 
     async showDialogExcelXlsx(arquivo: Arquivo) {
@@ -54,7 +54,7 @@ export class ExcelDialogComponent {
             arquivo.base64 || '',
             arquivo.nome
         );
-        this.loadExcelData(file);
+        await this.loadExcelData(file);
     }
 
     showDialogNovoExcelXlsx(arquivoList: Arquivo[]) {
@@ -62,8 +62,19 @@ export class ExcelDialogComponent {
         this.isEditar = false;
         this.header = 'Novo(a) Planilha do Microsoft Excel';
         this.displayExcel = true;
-        this.excelData.push(['-', '-'], ['-', '-']);
-        this.celulaEmEdicao = this.excelData.map((row) => [...row]);
+        this.excelData = [
+            {
+                sheetName: 'Sheet1',
+                data: [
+                    ['-', '-'],
+                    ['-', '-']
+                ]
+            }
+        ];
+        this.celulaEmEdicao = this.excelData.map((sheet) => ({
+            sheetName: sheet.sheetName,
+            data: sheet.data.map((row) => [...row])
+        }));
     }
 
     hideDialogEditor() {
@@ -71,7 +82,9 @@ export class ExcelDialogComponent {
         this.excelData = [];
         this.celulaEmEdicao = [];
         this.isEditar = true;
+        this.abaSelecionada = 0;
     }
+
     async loadExcelData(file: File): Promise<void> {
         const reader: FileReader = new FileReader();
 
@@ -81,21 +94,33 @@ export class ExcelDialogComponent {
                 const workbook: XLSX.WorkBook = XLSX.read(binaryStr, {
                     type: 'binary'
                 });
-                const firstSheetName: string = workbook.SheetNames[0];
-                const worksheet: XLSX.WorkSheet =
-                    workbook.Sheets[firstSheetName];
 
-                this.excelData = <any[][]>(
-                    XLSX.utils.sheet_to_json(worksheet, { header: 1 })
-                );
-                this.celulaEmEdicao = this.excelData.map((row) => [...row]);
+                this.excelData = [];
+                this.celulaEmEdicao = [];
+
+                workbook.SheetNames.forEach((sheetName: string) => {
+                    const worksheet: XLSX.WorkSheet =
+                        workbook.Sheets[sheetName];
+                    let sheetData: any[][] = <any[][]>(
+                        XLSX.utils.sheet_to_json(worksheet, { header: 1 })
+                    );
+
+                    // Ajusta a tabela para ter o mesmo número de colunas em todas as linhas
+                    sheetData =
+                        this.ajustarTabelaParaFormatoQuadrado(sheetData);
+
+                    this.excelData.push({ sheetName, data: sheetData });
+                    this.celulaEmEdicao.push({
+                        sheetName,
+                        data: sheetData.map((row) => [...row])
+                    });
+                });
             } catch (error) {
                 const errorMessage = (error as Error).message;
                 if (errorMessage.includes('password')) {
                     this.alertService.showWarningAlert(
                         'Excel protegido por senha.'
                     );
-                    // Aqui você pode disparar um diálogo SweetAlert2 para solicitar a senha.
                 } else {
                     this.alertService.showErrorAlert(
                         'Erro ao tentar ler o conteúdo do excel'
@@ -107,21 +132,27 @@ export class ExcelDialogComponent {
         reader.readAsBinaryString(file);
     }
 
-    onRowEditInit(rowIndex: number, colIndex: number) {
-        this.excelData[rowIndex][colIndex] =
-            this.celulaEmEdicao[rowIndex][colIndex];
+    onRowEditChange(
+        sheetIndex: number,
+        rowIndex: number,
+        colIndex: number
+    ): void {
+        this.excelData[sheetIndex].data[rowIndex][colIndex] =
+            this.celulaEmEdicao[sheetIndex].data[rowIndex][colIndex];
     }
 
-    onRowEditSave() {
-        // Atualize `excelData` com os valores de `celulaEmEdicao`
-        this.excelData = this.celulaEmEdicao.map((row) => [...row]);
+    onRowEditSave(): void {
+        this.excelData = this.celulaEmEdicao.map((sheet) => ({
+            sheetName: sheet.sheetName,
+            data: sheet.data.map((row) => [...row])
+        }));
 
-        // Gera a nova planilha Excel a partir dos dados atualizados
-        const ws: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet(this.excelData);
         const wb: XLSX.WorkBook = XLSX.utils.book_new();
-        XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+        this.excelData.forEach((sheet) => {
+            const ws: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet(sheet.data);
+            XLSX.utils.book_append_sheet(wb, ws, sheet.sheetName);
+        });
 
-        // Gerar o arquivo Excel
         const excelBuffer: any = XLSX.write(wb, {
             bookType: 'xlsx',
             type: 'array'
@@ -130,7 +161,6 @@ export class ExcelDialogComponent {
             type: 'application/octet-stream'
         });
 
-        // Cria o nome do arquivo usando o serviço `arquivoService`
         let nomeArquivo = this.arquivoService.concatenarNomeExtensaoArquivo(
             this.arquivo?.nome || '',
             this.arquivo?.extensao || ''
@@ -138,15 +168,13 @@ export class ExcelDialogComponent {
         if (!this.isEditar) {
             nomeArquivo = this.getNomeArquivoNovo();
         }
-        // Converte o Blob em um arquivo
+
         const file = new File([blob], nomeArquivo, {
             type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
         });
 
-        // Prepara os dados para enviar para o backend
         const formData = new FormData();
 
-        // Chama o serviço para enviar o arquivo atualizado
         if (this.isEditar) {
             formData.append('file', file);
             formData.append('nome', nomeArquivo);
@@ -167,17 +195,26 @@ export class ExcelDialogComponent {
         this.displayExcel = false;
     }
 
-    adicionarColuna() {
-        this.excelData.forEach((linha) => {
-            linha.push('-');
-        });
-        this.celulaEmEdicao = this.excelData.map((row) => [...row]);
+    adicionarColuna(sheetIndex: number): void {
+        this.excelData[sheetIndex].data.forEach((row) => row.push('-'));
+
+        this.celulaEmEdicao[sheetIndex] = {
+            sheetName: this.excelData[sheetIndex].sheetName,
+            data: this.excelData[sheetIndex].data.map((row) => [...row])
+        };
     }
-    adicionarLinha() {
-        let novaLinha: any = [];
-        this.excelData[0].forEach((linha) => novaLinha.push('-'));
-        this.excelData.push(novaLinha);
-        this.celulaEmEdicao = this.excelData.map((row) => [...row]);
+
+    adicionarLinha(sheetIndex: number): void {
+        const novaLinha = Array(this.excelData[sheetIndex].data[0].length).fill(
+            '-'
+        );
+
+        this.excelData[sheetIndex].data.push(novaLinha);
+
+        this.celulaEmEdicao[sheetIndex] = {
+            sheetName: this.excelData[sheetIndex].sheetName,
+            data: this.excelData[sheetIndex].data.map((row) => [...row])
+        };
     }
 
     getNomeArquivoNovo() {
@@ -193,5 +230,36 @@ export class ExcelDialogComponent {
             contador++;
         }
         return nomeArquivoNovo + extensao;
+    }
+
+    removeFocus() {
+        setTimeout(() => {
+            const activeElement = document.activeElement as HTMLElement;
+            if (activeElement) {
+                activeElement.blur();
+            }
+
+            const dialogContent = document.querySelector('.p-dialog-content');
+            if (dialogContent) {
+                dialogContent.scrollTop = 0;
+            }
+        });
+    }
+
+    ajustarTabelaParaFormatoQuadrado(tabela: any[][]): any[][] {
+        // Determina o comprimento da linha mais longa
+        const maxColumns = tabela.reduce(
+            (max, row) => Math.max(max, row.length),
+            0
+        );
+
+        // Adiciona células vazias nas linhas que têm menos colunas
+        return tabela.map((row) => {
+            const novaLinha = [...row];
+            while (novaLinha.length < maxColumns) {
+                novaLinha.push(''); // ou use null para células vazias
+            }
+            return novaLinha;
+        });
     }
 }
